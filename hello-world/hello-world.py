c31f284595232c93105009ae7b1d77241719708b
Andrea Arduino <andrea.arduino@outlook.com>
2020-04-29 12:54:12 +0200

    Docker: add /hello path and return JSON format

--- hello-world/hello-world.py
+++ hello-world/hello-world.py
@@ -1,3 +1,10 @@
+'''
 def application(environ, response):
     response('200 OK', [('Content-Type', 'text/html')])
     return [b"{ \"hello\": \"world\" }"]
+'''
+import json
+def application(environ, response):
+    response('200 OK', [('Content-Type', 'application/json')])
+    resp = bytes(json.dumps({'hello': 'world'}),'utf-8')
+    return [resp]
